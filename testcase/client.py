ad81f4cf9a0faa09e707f7c6bfe32dfec23df43a
Lukas RT <lukas.rosentreter@gmail.com>
2016-06-14 11:16:36 +0200

    Get Client to work

--- testcase/client.py
+++ testcase/client.py
@@ -2,18 +2,18 @@
 
 import requests
 
-"http://wiki.processmaker.com/3.0/Triggers#Enabling_Debug_Mode"
+from config import *
 
-# TODO: Retrieve constants from external config file
+# General
 
-PM_HOST      = "172.17.0.3"
-PM_WORKSPACE = "workflow"
 
-PM_USER = "unicorn"
-PM_PW   = "123456"
-
-CLIENT_ID     = "UCYFOVTKROBJVFWYBWJRQIPWFUNBZCAT"
-CLIENT_SECRET = "189803135575f01c37fb0b7099422018"
+def genAuthHeader():
+  jsn = getAccessToken()
+  if "error" in jsn:
+    print(jsn)
+    return
+  else:
+    return {"Authorization": "Bearer {tok}".format(tok=jsn["access_token"])}
 
 def  getAccessToken():
   url = "http://{host}/{workspace}/oauth2/token".format(host=PM_HOST, workspace=PM_WORKSPACE)
@@ -26,29 +26,44 @@ def  getAccessToken():
     "password": PM_PW
   }
   r = requests.post(url, data=payload)
-
   # Stop if status isn't 200 OK
   r.raise_for_status()
-
-  jsn = r.json()
-
-  if "error" in jsn:
-    print (jsn)
-    return
-  else:
-    return r.json()
+  return r.json()
 
 
-def getProjectsList():
-    "http://wiki.processmaker.com/3.0/REST_API_Designer#get_projects"
+# Starting New Case
 
-def getStartingTasks():
-    "http://wiki.processmaker.com/3.0/REST_API_Designer#Get_starting_tasks"
+def getProjectsList(auth):
+  url = "http://{host}/api/1.0/{workspace}/project".format(host=PM_HOST, workspace=PM_WORKSPACE)
+  r = requests.get(url, headers=auth)
+  return r.json()
+
+def getStartingTasks(auth, prj):
+  url = "http://{host}/api/1.0/{workspace}/project/{prj_uid}/starting-tasks" \
+    .format(host=PM_HOST, workspace=PM_WORKSPACE, prj_uid=prj["prj_uid"])
+  r = requests.get(url, headers=auth)
+  return r.json()
+
+def listProjects(jsn):
+  print("UID                              | Name")
+  for proj in jsn:
+    print("{uid} | {name}".format(uid=proj["prj_uid"], name=proj["prj_name"]))
+
+def listTasks(jsn):
+  print("UID                              | Name")
+  for tsk in jsn:
+    print("{uid} | {name}".format(uid=tsk["act_uid"], name=tsk["act_name"]))
+
+def listAllStartingTasks(auth):
+  pl = getProjectsList(auth)
+  for prj in pl:
+    print("\n\nProject:\n{uid} | {name}\n".format(uid=prj["prj_uid"], name=prj["prj_name"]))
+    st = getStartingTasks(auth, prj)
+    listTasks(st)
 
 
-def startCase(access, pro_uid, tas_uid):
+def startCase(auth, pro_uid, tas_uid):
   url = "http://{host}/api/1.0/{workspace}/cases".format(host=PM_HOST, workspace=PM_WORKSPACE)
-  auth = {"Authorization": access["access_token"]}
   payload = {
     "pro_uid": pro_uid,
     "tas_uid": tas_uid
@@ -56,11 +71,10 @@ def startCase(access, pro_uid, tas_uid):
   r = requests.post(url, headers=auth, data=payload)
   return r
 
-def routeCase(access, app_uid):
-  url = "http://{host}/api/1.0/{workspace}/cases/{app_uid}/route-case"
+def routeCase(auth, app_uid):
+  url = "http://{host}/api/1.0/{workspace}/cases/{app_uid}/route-case" \
     .format(host=PM_HOST, workspace=PM_WORKSPACE, app_uid=app_uid)
-  auth = {"Authorization": access["access_token"]}
-  r = requests.put(url, headers=auth, data=payload)
+  r = requests.put(url, headers=auth)
   return r
 
 if __name__ == "__main__":
