84579759ab24153815ece09fdad196869d6c761d
Lukas RT <lukas.rosentreter@gmail.com>
2016-06-14 19:48:05 +0200

    Rewrite Python client in object orientated manner

--- testcase/client.py
+++ testcase/client.py
@@ -1,81 +1,120 @@
 #! python
 
 import requests
+import config
 
-from config import *
+class ProcessmakerWrapper:
+  """Wrapper-Class for the Processmaker API"""
 
-# General
+  def __init__(self, config):
+    self.c = config
+    self.base_url = "http://{host}/api/1.0/{workspace}" \
+      .format(host=self.c.PM_HOST, workspace=self.c.PM_WORKSPACE)
+    self.auth = self.genAuthHeader():
+    self.updateProcessData()
 
+  def pushInitial(self, uid):
+    task = self.start_tasks[uid]
 
-def genAuthHeader():
-  jsn = getAccessToken()
-  if "error" in jsn:
-    print(jsn)
-    return
-  else:
-    return {"Authorization": "Bearer {tok}".format(tok=jsn["access_token"])}
+    case_uid = self.startCase(task)
 
-def  getAccessToken():
-  url = "http://{host}/{workspace}/oauth2/token".format(host=PM_HOST, workspace=PM_WORKSPACE)
-  payload = {
-    "grant_type": "password",
-    "scope": "*",
-    "client_id": CLIENT_ID,
-    "client_secret": CLIENT_SECRET,
-    "username": PM_USER,
-    "password": PM_PW
-  }
-  r = requests.post(url, data=payload)
-  # Stop if status isn't 200 OK
-  r.raise_for_status()
-  return r.json()
+    # put some required variables in case..
 
+    self.routeCase(case_uid)
 
-# Starting New Case
+  def pullIntermediate(self):
+    # get case inbox (GET /cases)
+    # is there some?
+    # -> save app_uid somewhere
+    # get variables and stuff
+    # send id, variables etc to unicorn
+    # (maybe) pause case (PUT /cases/app_uid/pause)
+    # or send to draft (happens automatically,
+    # when some step of current task in case is completed)
 
-def getProjectsList(auth):
-  url = "http://{host}/api/1.0/{workspace}/project".format(host=PM_HOST, workspace=PM_WORKSPACE)
-  r = requests.get(url, headers=auth)
-  return r.json()
+  def pushIntermediate(self, case_uid):
+    # case_uid should be saved somewhere
+    # also, case should be paused or in draft
+    # set variables and stuff
+    # maybe unpause case
+    # route case
 
-def getStartingTasks(auth, prj):
-  url = "http://{host}/api/1.0/{workspace}/project/{prj_uid}/starting-tasks" \
-    .format(host=PM_HOST, workspace=PM_WORKSPACE, prj_uid=prj["prj_uid"])
-  r = requests.get(url, headers=auth)
-  return r.json()
-
-def listProjects(jsn):
-  print("UID                              | Name")
-  for proj in jsn:
-    print("{uid} | {name}".format(uid=proj["prj_uid"], name=proj["prj_name"]))
-
-def listTasks(jsn):
-  print("UID                              | Name")
-  for tsk in jsn:
-    print("{uid} | {name}".format(uid=tsk["act_uid"], name=tsk["act_name"]))
-
-def listAllStartingTasks(auth):
-  pl = getProjectsList(auth)
-  for prj in pl:
-    print("\n\nProject:\n{uid} | {name}\n".format(uid=prj["prj_uid"], name=prj["prj_name"]))
-    st = getStartingTasks(auth, prj)
-    listTasks(st)
+  def pullFinished(self):
+    # seems to only work like pullIntermediate
+    # -> task at the end of process, assigned to unicorn user
 
 
-def startCase(auth, pro_uid, tas_uid):
-  url = "http://{host}/api/1.0/{workspace}/cases".format(host=PM_HOST, workspace=PM_WORKSPACE)
-  payload = {
-    "pro_uid": pro_uid,
-    "tas_uid": tas_uid
-  }
-  r = requests.post(url, headers=auth, data=payload)
-  return r
+  def genAuthHeader(self):
+    jsn = self.getAccessToken()
+    if "error" in jsn:
+      print(jsn)
+      return
+    else:
+      return {"Authorization": "Bearer {tok}".format(tok=jsn["access_token"])}
 
-def routeCase(auth, app_uid):
-  url = "http://{host}/api/1.0/{workspace}/cases/{app_uid}/route-case" \
-    .format(host=PM_HOST, workspace=PM_WORKSPACE, app_uid=app_uid)
-  r = requests.put(url, headers=auth)
-  return r
+  def  getAccessToken(self):
+    url = "http://{host}/{workspace}/oauth2/token" \
+      .format(host=self.c.PM_HOST, workspace=self.c.PM_WORKSPACE)
+    payload = {
+      "grant_type": "password",
+      "scope": "*",
+      "client_id": self.c.CLIENT_ID,
+      "client_secret": self.c.CLIENT_SECRET,
+      "username": self.c.PM_USER,
+      "password": self.c.PM_PW
+    }
+    r = requests.post(url, data=payload)
+    # Stop if status isn't 200 OK
+    r.raise_for_status()
+    return r.json()
+
+  def updateProcessData(self):
+    prj_list = self.getProjectList()
+    self.processes   = {}
+    self.start_tasks = {}
+    for prj in prj_list:
+      self.processes[prj["uid"]] = prj
+      tasks = self.getStartingTasks(prj["uid"])
+      for t in tasks:
+        self.start_tasks[t["uid"]] = t
+
+  def getProjectList(self):
+    url = "{base}/project".format(base=self.base_url)
+    r = requests.get(url, headers=self.auth)
+    # TODO Exception- and Error-Code-handling
+    return [{"uid": prj["prj_uid"], "name": prj["prj_name"] } for prj in r.json()]
+
+  def getStartingTasks(self, process_uid):
+    url = "{base}/project/{prj_uid}/starting-tasks" \
+      .format(base=self.base_url, prj_uid=process_uid)
+    r = requests.get(url, headers=self.auth)
+    # TODO Exception- and Error-Code-handling
+    return [{"uid": tsk["act_uid"], "name": tsk["act_uid"], "process": process_uid} for tsk in r.json()]
+
+  def startCase(self, task):
+    url = "{base}/cases".format(base=self.base_url)
+    payload = {
+      "pro_uid": task["process"],
+      "tas_uid": task["uid"]
+    }
+    r = requests.post(url, headers=self.auth, data=payload)
+    return r.json()["app_uid"]
+
+  def routeCase(self, case_uid):
+    url = "{base}/cases/{app_uid}/route-case" \
+      .format(base=self.base_url, app_uid=case_uid)
+    r = requests.put(url, headers=self.auth)
+    return r
 
 if __name__ == "__main__":
-  print("this is a lib module")
+  pmw = ProcessmakerWrapper(config)
+
+  tasks = pmw.start_tasks
+  example = tasks.keys()[0]
+  # NOTE: The above shouldn't be the usual usecase.
+  # When starting cases based on matched queries in unicorn,
+  # the task/case id should either come from
+  # manual mapping/configuration done beforehand or
+  # or be derived from the matched query.
+
+  pmw.pushInitial(example)
