c104f5a65f6677cc7dbb3bbc891e07887dcee73d
Lukas RT <lukas.rstr@gmail.com>
2016-07-18 16:49:05 +0200

    Refactor pm rest client

--- connector/pmucon/rest.py
+++ connector/pmucon/rest.py
@@ -2,53 +2,35 @@
 
 import requests
 
-class Rest(object):
-    """Class for calling any PM REST endpoints."""
+def get(url, hdr):
+  return request('get', url, hdr)
 
-    def __init__(self, host):
-        self.host = host
+def post(url, hdr, payload):
+  return request('post', url, hdr, payload)
 
-    def request(self, method, endpoint, data = "", access_token = ""):
+def put(url, hdr):
+  return request('put', url, hdr)
 
-        if not access_token:
-            print("No valid access token given.")
-            return False
 
-        if endpoint and endpoint[0] is not "/":
-            endpoint = "/"+endpoint
+def request(method, url, hdr, payload=""):
+  if(method is "get"):
+    r = requests.get(url, headers=hdr)
+  elif(method is "post"):
+    r = requests.post(url, headers=hdr, data=payload)
+  elif(method is "put"):
+    r = requests.put(url, headers=hdr)
+  else:
+    raise Exception("No valid HTTP function was given. \"{method}\" Aborting." \
+      .format(method = method))
 
-        url    = self.host + endpoint
-        header = {"Authorization": "Bearer {token}".format(token=access_token)}
-
-        if(method is "get"):
-            r = self.get(url, header)
-        elif(method is "post"):
-            r = self.post(url, header, data)
-        elif(method is "put"):
-            r = self.put(url, header)
-        else:
-            print("No valid HTTP function was given. \"{method}\" Aborting." \
-                .format(method = method))
-            return
-
-        if r.status_code == 401:
-            print("Bad login. Please check your login Info.")
-            return
-        elif r.status_code != 200 and r.status_code != 201:
-            print("Error in {host}. The server replied: \n {code}: {msg}" \
-                .format(host = self.host, code=r.status_code, msg=r.json()["error"]))
-            return
-        else:
-            try:
-                return r.json()
-            except:
-                return r
-
-    def get(self, url, header):
-        return requests.get(url, headers=header)
-
-    def post(self, url, header, payload):
-        return requests.post(url, headers=header, data=payload)
-
-    def put(self, url, header):
-        return requests.put(url, headers=header)
+  if r.status_code == 401:
+    raise Exception("Bad login. Please check your login Info.")
+  elif r.status_code != 200 and r.status_code != 201:
+    err = "Error in {url}. The server replied: \n {code}: {msg}" \
+      .format(host = self.host, code=r.status_code, msg=r.json()["error"])
+    raise Exception(err)
+  else:
+    try:
+      return r.json()
+    except:
+      return r
